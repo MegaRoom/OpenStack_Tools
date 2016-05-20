b03a4104e0c0de4053e1bc2f03bea7794291131c
Hiroaka Kojima <hiroakakojima@Hiroaka-no-MacBook-Air.local>
2016-05-20 16:41:27 +0900

    change filename of the screenshot

--- vnc_screen/take_screen_shots/vnc_screen.py
+++ vnc_screen/take_screen_shots/vnc_screen.py
@@ -21,6 +21,7 @@ def TakeScreenShot(URL, FILENAME, PATH):
   driver.find_element_by_tag_name('body').send_keys(Keys.SHIFT)
   # Get Screen Shot
   driver.save_screenshot(ScreenShotPath)
+  print 'saved screent shot to '  + ScreenShotPath
   # Close Web Browser
   driver.quit()
 
@@ -39,7 +40,8 @@ if __name__=="__main__":
   #Set Paramater
   URL = argvs[1]
   CURRENT = os.path.dirname(os.path.abspath(__file__))
-  FILENAME =  re.compile("^.*token=").sub("",URL) + ".png"
+  #FILENAME =  re.compile("^.*token=").sub("",URL) + ".png"
+  FILENAME = "{0:%Y-%m-%d_%H:%M:%S}".format(datetime.datetime.now()) + '.png'
   TODAY = str(datetime.date.today())
   PATH = os.path.join(CURRENT, 'screenshot')
   PATH = os.path.join(PATH, TODAY)
